33f2cd769f4d5622c56b309a815e005923eaf2b1
Krishna Patil <krishnacpatil2006@gmail.com>
2026-04-03 17:20:42 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,12 +1,22 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: '/api' });
+// Development service ports
+const AUTH_URL   = 'http://localhost:3001/api/auth';
+const EDITOR_URL = 'http://localhost:3002/api';
+const AGENT_URL  = 'http://localhost:3003/api/agents';
 
-// Automatically attach JWT token to every request
-API.interceptors.request.use((config) => {
-  const token = localStorage.getItem('kiri_token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
+export const AuthAPI   = axios.create({ baseURL: AUTH_URL });
+export const EditorAPI = axios.create({ baseURL: EDITOR_URL });
+export const AgentAPI  = axios.create({ baseURL: AGENT_URL });
+
+const apis = [AuthAPI, EditorAPI, AgentAPI];
+
+apis.forEach(api => {
+  api.interceptors.request.use((config) => {
+    const token = localStorage.getItem('kiri_token');
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  });
 });
 
-export default API;
+export default { AuthAPI, EditorAPI, AgentAPI };
